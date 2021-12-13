523daa7081f7bc99b0405529c09a3d7cac8c305f
Mrkoutoku <95897405+Mrkoutoku@users.noreply.github.com>
2021-12-13 13:51:19 +0900

    Add files via upload

---
+++ new.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<title>
+    勝活
+</title>
+<link rel="stylesheet" href="./reset.css">
+<link rel="stylesheet" href="./new.css">
+</head>
+<body>
+<div class="main">
+   <h1>勝活</h1>
+   
+ <div class="waku">
+      <form method="post" action="keisan.php">
+            <h2>プレイヤーはニックネームを入力してください</h2><br/>
+            <input name="nickname1" type="text" style="width:100px">
+            <input name="nickname2" type="text" style="width:100px">
+            <input name="nickname3" type="text" style="width:100px">
+            <input name="nickname4" type="text" style="width:100px"><br/><br/>
+            <input type="submit" value="開始"> 
+      </form>
+ </div>
+</div> 
+
+
+</body>
+</html>
